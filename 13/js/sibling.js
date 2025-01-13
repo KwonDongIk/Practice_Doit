2175ba21be66985597afe38961376b29661eec4b
KwonDongIk <dongikik@kakao.com>
2025-01-13 23:23:45 +0900

    0113 practice commit

---
+++ 13/js/sibling.js
@@ -0,0 +1,8 @@
+let myHeight = 160;
+let myAge = 25;
+let myItem = document.querySelector('section h1+p');
+myItem.addEventListener('click', function() {alert("클릭했습니다."); })
+
+console.log(alert("Hello world"));
+console.log("hi");
+console.log(myItem);
